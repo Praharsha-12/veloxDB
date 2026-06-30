9e407628bc8d3f0da6e52db794837afdb8b097fc
Praharsha Vemula <praharsha256@gmail.com>
2026-06-30 15:22:21 +0530

    Add files via upload

---
+++ frontend1/public/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h2>VeloxDB</h2>
+
+      <div className="user">
+        Admin
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
